d58d051eb322cc2798227baf3071dfd4f5327c9b
Timeout <taubmann870@gmail.com>
2018-11-03 15:21:25 +0100

    Add Libs from de.timeout.Libs

---
+++ src/de/rytrox/ttt/TTT.java
@@ -0,0 +1,36 @@
+package de.rytrox.ttt;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import de.timeout.utils.UTFConfig;
+
+public class TTT extends JavaPlugin {
+	
+	private UTFConfig config;
+
+	@Override
+	public void onEnable() {
+
+	}
+	
+	@Override
+	public void onDisable() {
+
+	}
+
+	@Override
+	public void reloadConfig() {
+
+	}
+
+	@Override
+	public void saveConfig() {
+
+	}
+	
+	@Override
+	public UTFConfig getConfig() {
+		return config;
+	}
+
+}
